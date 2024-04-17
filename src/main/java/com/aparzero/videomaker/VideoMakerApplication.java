ee25ce4a109413edc765f95c1539669db0bfac2a
aparzero30 <jimcin30@gmail.com>
2024-04-17 11:22:56 +0800

    initial commit

---
+++ src/main/java/com/aparzero/videomaker/VideoMakerApplication.java
@@ -0,0 +1,13 @@
+package com.aparzero.videomaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aparzero/videomaker/VideoMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aparzero.videomaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
